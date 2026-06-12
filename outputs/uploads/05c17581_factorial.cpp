4e68b6f980aedb250976916505dd31fa9e66afd9
SounjnaBhat <bhatsounjna@gmail.com>
2026-06-12 21:25:59 +0530

    resolved some error

--- outputs/uploads/05c17581_factorial.cpp
+++
@@ -1,9 +0,0 @@
-int factorial(int n) {
-    int val = 1;
-    int i = 1;
-    while (i <= n) {
-        val *= i;
-        i++;
-    }
-    return val;
-}
--- outputs/uploads/0ec4b476_factorial.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int factorial(int n) {
-    int val = 1;
-    for (int i = 1; i <= n; i++) {
-        val *= i;
-    }
-    return val;
-}
